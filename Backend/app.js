aa5d252555bb46fcc2508f96754c30dbb7545e2d
patelkrati00 <patelkrati00@gmail.com>
2025-08-25 19:36:14 +0530

    setup backend

---
+++ Backend/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { createServer } from "node:http";
+import connectToSocket from './src/controllers/socketManager.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({ limit: "40kb", extended: false }));
+
+const start = async () => {
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected");
+    server.listen(app.get("port"), () => {
+        console.log('Server is running on port 3000');
+    });
+}
+start();
